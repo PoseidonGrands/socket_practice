f15a9ef51f04bd038946b6f37fc4a05ef5b152eb
sewellhe <sewellhe@outlook.com>
2024-03-25 23:41:34 +0800

    了解基于http的数据传送

---
+++ my_socket/http_server.py
@@ -0,0 +1,43 @@
+import socket
+
+from concurrent.futures import ThreadPoolExecutor
+
+
+t = ThreadPoolExecutor(5)
+
+# 默认是tcp协议
+server = socket.socket()
+server.bind(('0.0.0.0', 8000))
+server.listen()
+
+
+# 对每个请求的处理
+def handle_sock(_sock, addr):
+    data = ""
+    while True:
+        tmp_data = _sock.recv(1024)
+
+        response_template = '''HTTP/1.1 200 OK
+
+<html>
+<head><title>hhhh</title></head>
+<body><h1>hello</h1></body>
+</html>
+
+'''
+        _sock.send(response_template.encode('utf-8'))
+
+# 不断接受客户端请求，并新开线程处理
+while True:
+    # server负责接受连接请求，sock负责具体的数据传输，而addr则提供了客户端的地址信息（ip、端口
+    # 阻塞方法，如果客户端close服务端会接收并取消阻塞
+    sock, addr = server.accept()
+    print('client is connected...')
+    t.submit(handle_sock, sock, addr)
+
+
+# sock.close()
+
+
+
+
